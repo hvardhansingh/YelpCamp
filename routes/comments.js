d071376c398415cee12d922b7bcd6c867442089b
hvardhansingh <harshvs.cd.mec17@itbhu.ac.in>
2020-03-23 15:50:21 +0530

    Add edit comment and delete comment routes

--- routes/comments.js
+++ routes/comments.js
@@ -43,6 +43,49 @@ router.post('/', isLoggedIn, function (req, res) {
     });
 });
 
+// EDIT Route
+// campgrounds/:id/comments/:id/edit
+router.get('/:comId/edit', function(req, res){
+    
+    Comment.findById(req.params.comId, function(err, foundComment){
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.render('comments/edit.ejs', {campgroundId: req.params.id, comment: foundComment});
+        }
+    });
+    
+});
+
+// UPDATE Route
+// campgrounds/:id/comments/:id/
+router.put('/:commentId', function(req, res){
+    Comment.findByIdAndUpdate(req.params.commentId, req.body.comment,  function(err, updatedComment){
+        if(err){
+            console.log(err);
+        }
+        else{
+            console.log('Comment Updated !');
+            res.redirect('/campgrounds/'+req.params.id);
+        }
+    });
+}); 
+
+// DESTROY Route
+// campgrounds/:id/comments/:commentId
+
+router.delete('/:commentId', function(req, res){
+    Comment.findByIdAndRemove(req.params.commentId, function(err, removedComment){
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.redirect('/campgrounds/'+req.params.id);
+        }
+    });
+})
+
 function isLoggedIn(req, res, next){
     if(req.isAuthenticated()){
         return next();
